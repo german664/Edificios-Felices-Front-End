0c7bc6a98b7db7e84171aaec2b66960ff321da45
German Mora <german.mora.664@gmail.com>
2020-10-28 19:21:23 -0300

    min height

--- src/js/layout.js
+++ src/js/layout.js
@@ -41,40 +41,44 @@ const Layout = () => {
     return (
         <>
             <BrowserRouter>
-                <Switch>
-                    <Route exact path='/usuario/paqueteria' component={PaqueteriaUser} />
-                    <Route exact path='/admin/historial-boletines' component={HistorialBoletin} />
-                    <Route exact path='/admin/gastos-depto-actual' component={GastosDeptoActual} />
-                    <Route exact path='/admin/gastos-departamentos' component={GastosDepartamentos} />
-                    <Route exact path='/admin/subir-gastos' component={SubirGastos} />
-                    <Route exact path='/admin/inicializacion-pisos' component={InicializacionPisos} />
-                    <Route exact path='/admin/inicializacion-dptos' component={InicializacionDptos} />
-                    <Route exact path='/boletinAdm' component={BoletinAdm} />
-                    <Route exact path='/contratos' component={Contratos} />
-                    <Route exact path='/allusuarios' component={Usuarios} />
-                    <Route exact path='/usuario/gastos-comunes' component={GastosUser} />
-                    <Route exact path='/conserje/gastos-comunes' component={GastosConserje} />
-                    <Route exact path='/conserje/paqueteria' component={PaqueteriaConserje} />
-                    <Route exact path='/boletines' component={BoletinesConserje} />
-                    <Route exact path='/conserje/departamentos' component={DepartamentosConserje} />
-                    <Route exact path='/admin/gastos-comunes' component={GastosAdmin} />
-                    <Route exact path='/admin/boletines' component={BoletinesAdmin} />
-                    <Route exact path='/admin/departamentos' component={DepartamentosAdmin} />
-                    <Route exact path='/conserjes' component={Conserjes} />
-                    <Route exact path='/contactados' component={Contactados} />
-                    <Route exact path='/contactos' component={Contactos} />
-                    <Route exact path='/listado-edificios' component={ListadoEdificioAdm} />
-                    <Route exact path='/listado-edificios/:id' component={DetalleEdificio} />
-                    <Route exact path='/dashboard' component={Dashboard} />
-                    <Route exact path='/recuperacion-password/:token' component={RecuperacionContraseña} />
-                    <Route exact path='/crearedificio' component={CrearEdificios} />
-                    <Route exact path='/login' component={Login} />
-                    <Route exact path='/' component={Home} />
-                    <Route exact path='/planes' component={Planes} />
-                    <Route exact path='/sidebar' component={SidebarPage} />
-                    <Route exact path='/superAdmin/planes' component={ModifyPlans} />
-                    <Route render={() => <h1 className="notfound">Not found!</h1>} />
-                </Switch>
+                <main>
+                    <Switch>
+
+                        <Route exact path='/usuario/paqueteria' component={PaqueteriaUser} />
+                        <Route exact path='/admin/historial-boletines' component={HistorialBoletin} />
+                        <Route exact path='/admin/gastos-depto-actual' component={GastosDeptoActual} />
+                        <Route exact path='/admin/gastos-departamentos' component={GastosDepartamentos} />
+                        <Route exact path='/admin/subir-gastos' component={SubirGastos} />
+                        <Route exact path='/admin/inicializacion-pisos' component={InicializacionPisos} />
+                        <Route exact path='/admin/inicializacion-dptos' component={InicializacionDptos} />
+                        <Route exact path='/boletinAdm' component={BoletinAdm} />
+                        <Route exact path='/contratos' component={Contratos} />
+                        <Route exact path='/allusuarios' component={Usuarios} />
+                        <Route exact path='/usuario/gastos-comunes' component={GastosUser} />
+                        <Route exact path='/conserje/gastos-comunes' component={GastosConserje} />
+                        <Route exact path='/conserje/paqueteria' component={PaqueteriaConserje} />
+                        <Route exact path='/boletines' component={BoletinesConserje} />
+                        <Route exact path='/conserje/departamentos' component={DepartamentosConserje} />
+                        <Route exact path='/admin/gastos-comunes' component={GastosAdmin} />
+                        <Route exact path='/admin/boletines' component={BoletinesAdmin} />
+                        <Route exact path='/admin/departamentos' component={DepartamentosAdmin} />
+                        <Route exact path='/conserjes' component={Conserjes} />
+                        <Route exact path='/contactados' component={Contactados} />
+                        <Route exact path='/contactos' component={Contactos} />
+                        <Route exact path='/listado-edificios' component={ListadoEdificioAdm} />
+                        <Route exact path='/listado-edificios/:id' component={DetalleEdificio} />
+                        <Route exact path='/dashboard' component={Dashboard} />
+                        <Route exact path='/recuperacion-password/:token' component={RecuperacionContraseña} />
+                        <Route exact path='/crearedificio' component={CrearEdificios} />
+                        <Route exact path='/login' component={Login} />
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/planes' component={Planes} />
+                        <Route exact path='/sidebar' component={SidebarPage} />
+                        <Route exact path='/superAdmin/planes' component={ModifyPlans} />
+                        <Route render={() => <h1 className="notfound">Not found!</h1>} />
+
+                    </Switch>
+                </main>
                 <Footer />
             </BrowserRouter>
         </>
